14d1dafb4944485f4aeca08709c6bfa5787f6b67
Rasmus Bruhn Nielsen <rasmusbruhnnielsen@gmail.com>
2026-03-22 18:43:13 +0100

    Added descriptions to the bracket auto-complete commands

--- init.lua
+++ init.lua
@@ -206,12 +206,12 @@ vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper win
 -- vim.keymap.set("n", "<C-S-k>", "<C-w>K", { desc = "Move window to the upper" })
 
 -- Git command shortcuts
-vim.keymap.set('i', "'", "''<Left>")
-vim.keymap.set('i', '"', '""<Left>')
-vim.keymap.set('i', '(', '()<Left>')
-vim.keymap.set('i', '[', '[]<Left>')
-vim.keymap.set('i', '{', '{}<Left>')
-vim.keymap.set('i', '<', '<><Left>')
+vim.keymap.set('i', "'", "''<Left>", { desc = 'Auto-complete quotes' })
+vim.keymap.set('i', '"', '""<Left>', { desc = 'Auto-complete double quotes' })
+vim.keymap.set('i', '(', '()<Left>', { desc = 'Auto-complete brackets' })
+vim.keymap.set('i', '[', '[]<Left>', { desc = 'Auto-complete square brackets' })
+vim.keymap.set('i', '{', '{}<Left>', { desc = 'Auto-complete curly brackets' })
+vim.keymap.set('i', '<', '<><Left>', { desc = 'Auto-complete sharp brackets' })
 
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
